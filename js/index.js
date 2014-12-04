514b3f9eaf89a505dbbd1a8579e3ba429b8c008a
Ahmed Matar <amatar07@live.com>
2014-12-04 19:18:53 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+
+
+//adding highlight to menu selection
+    $('nav a, .nav2 a').on('click', function() {
+  $(this).addClass('active');  $(this).parents('li').siblings().children('a').removeClass('active');
+});
+
+
+
+$( ".academics" ).click(function() {
+  $(".academicsSUB").toggle();
+});
+
+$( ".extra" ).click(function() {
+  $(".extraSUB").toggle();
+});
+
+$( ".agenda" ).click(function() {
+  $(".agendaSUB").toggle();
+});
+
+$( ".about" ).click(function() {
+  $(".aboutSUB").toggle();
+});
+
+
+});
