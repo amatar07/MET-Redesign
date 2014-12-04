0c34407481bd30c7eb14cecbdffe63aee698eb53
Karim <cg.karimtarek@gmail.com>
2014-12-04 21:19:15 +0200

    Dashboard

--- js/index.js
+++ js/index.js
@@ -1,28 +1,24 @@
 $( document ).ready(function() {
 
+	//adding highlight to menu selection
+	$('nav a, .nav2 a').on('click', function() {
+		$(this).addClass('active');  $(this).parents('li').siblings().children('a').removeClass('active');
+	});
 
-//adding highlight to menu selection
-    $('nav a, .nav2 a').on('click', function() {
-  $(this).addClass('active');  $(this).parents('li').siblings().children('a').removeClass('active');
-});
+	$( ".academics" ).click(function() {
+		$(".academicsSUB").toggle();
+	});
 
+	$( ".extra" ).click(function() {
+		$(".extraSUB").toggle();
+	});
 
+	$( ".agenda" ).click(function() {
+		$(".agendaSUB").toggle();
+	});
 
-$( ".academics" ).click(function() {
-  $(".academicsSUB").toggle();
-});
-
-$( ".extra" ).click(function() {
-  $(".extraSUB").toggle();
-});
-
-$( ".agenda" ).click(function() {
-  $(".agendaSUB").toggle();
-});
-
-$( ".about" ).click(function() {
-  $(".aboutSUB").toggle();
-});
-
+	$( ".about" ).click(function() {
+		$(".aboutSUB").toggle();
+	});
 
 });
